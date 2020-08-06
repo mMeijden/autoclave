91ca480e324d535a5846ae28112e1c68cf218ed4
Matthijs van der Meijden <matthijs.van.der.meijden@nnip.com>
2020-08-06 11:31:54 +0200

    updated to latest owasp version and clojure versions

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :codox {:output-path "doc/codox"
           :source-uri "http://github.com/alxlit/autoclave/blob/{version}/{filepath}#L{line}"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.pegdown/pegdown "1.6.0"]
                  [commons-codec "1.4"]
                  [com.google.guava/guava "11.0"]
                  [com.mikesamuel/json-sanitizer "1.1"
                   :exclusions [com.google.guava/guava
                                com.google.code.findbugs/jsr305]]
-                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20160203.1"
+                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20200713.1"
                   :exclusions [commons-codec
                                com.google.guava/guava
                                com.google.code.findbugs/jsr305]]])
